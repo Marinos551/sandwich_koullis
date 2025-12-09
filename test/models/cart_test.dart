33a95f51eb00a74daea604c70c9959adf20ade26
Marinos Pierides <pieridesmarinos@gmail.com>
2025-12-09 18:12:22 +0000

    test: Add comprehensive tests for Cart functionality including add, remove, clear, and quantity management

--- test/models/cart_test.dart
+++ test/models/cart_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sandwich_koullis/models/cart.dart';
+import 'package:sandwich_koullis/models/sandwich.dart';
 
 void main() {
   group('Cart', () {
@@ -7,6 +8,53 @@ void main() {
       final cart = Cart();
       expect(cart, isNotNull);
     });
-    // Add more Cart tests here
+
+    test('Add sandwich to cart', () {
+      final cart = Cart();
+      final sandwich = Sandwich(type: SandwichType.veggieDelight, isFootlong: true, breadType: BreadType.white);
+      cart.add(sandwich, quantity: 2);
+      expect(cart.items[sandwich], 2);
+      expect(cart.countOfItems, 2);
+    });
+
+    test('Remove sandwich from cart', () {
+      final cart = Cart();
+      final sandwich = Sandwich(type: SandwichType.tunaMelt, isFootlong: false, breadType: BreadType.wheat);
+      cart.add(sandwich, quantity: 3);
+      cart.remove(sandwich, quantity: 2);
+      expect(cart.items[sandwich], 1);
+      cart.remove(sandwich);
+      expect(cart.items.containsKey(sandwich), false);
+    });
+
+    test('Clear cart', () {
+      final cart = Cart();
+      final sandwich1 = Sandwich(type: SandwichType.chickenTeriyaki, isFootlong: true, breadType: BreadType.wholemeal);
+      final sandwich2 = Sandwich(type: SandwichType.meatballMarinara, isFootlong: false, breadType: BreadType.white);
+      cart.add(sandwich1, quantity: 1);
+      cart.add(sandwich2, quantity: 2);
+      cart.clear();
+      expect(cart.isEmpty, true);
+      expect(cart.countOfItems, 0);
+    });
+
+    test('Increment and decrement quantity', () {
+      final cart = Cart();
+      final sandwich = Sandwich(type: SandwichType.veggieDelight, isFootlong: true, breadType: BreadType.white);
+      cart.add(sandwich, quantity: 1);
+      cart.incrementQuantity(0);
+      expect(cart.items[sandwich], 2);
+      cart.decrementQuantity(0);
+      expect(cart.items[sandwich], 1);
+    });
+
+    test('Total price calculation', () {
+      final cart = Cart();
+      final sandwich1 = Sandwich(type: SandwichType.chickenTeriyaki, isFootlong: true, breadType: BreadType.wholemeal);
+      final sandwich2 = Sandwich(type: SandwichType.meatballMarinara, isFootlong: false, breadType: BreadType.white);
+      cart.add(sandwich1, quantity: 2);
+      cart.add(sandwich2, quantity: 1);
+      expect(cart.totalPrice, greaterThan(0));
+    });
   });
 }
