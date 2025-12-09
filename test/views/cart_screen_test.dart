66a571d65529e58871d2002b0698cfee99342231
Marinos Pierides <pieridesmarinos@gmail.com>
2025-12-09 18:13:01 +0000

    test: Add widget tests for CartScreen to verify cart display and checkout button

--- test/views/cart_screen_test.dart
+++ test/views/cart_screen_test.dart
@@ -2,6 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:sandwich_koullis/views/cart_screen.dart';
 import 'package:sandwich_koullis/models/cart.dart';
 import 'package:flutter/material.dart';
+import 'package:sandwich_koullis/views/styled_button.dart';
+import 'package:sandwich_koullis/models/sandwich.dart';
 
 void main() {
   testWidgets('CartScreen shows Your Cart', (WidgetTester tester) async {
@@ -12,5 +14,27 @@ void main() {
       ),
     );
     expect(find.text('Your Cart'), findsOneWidget);
+  });
+
+  testWidgets('CartScreen shows empty cart message', (WidgetTester tester) async {
+    final cart = Cart();
+    await tester.pumpWidget(
+      MaterialApp(
+        home: CartScreen(cart: cart),
+      ),
+    );
+    expect(find.textContaining('empty'), findsOneWidget);
+  });
+
+  testWidgets('CartScreen has checkout button', (WidgetTester tester) async {
+    final cart = Cart();
+    final sandwich = Sandwich(type: SandwichType.veggieDelight, isFootlong: true, breadType: BreadType.white);
+    cart.add(sandwich, quantity: 1);
+    await tester.pumpWidget(
+      MaterialApp(
+        home: CartScreen(cart: cart),
+      ),
+    );
+    expect(find.text('Checkout'), findsOneWidget);
   });
 }
