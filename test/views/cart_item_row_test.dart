ce8558b73158238b94b440570974da99f6631f27
Marinos Pierides <pieridesmarinos@gmail.com>
2025-12-09 17:47:48 +0000

    test: Implement CartItemRow rendering test to verify sandwich name display

--- test/views/cart_item_row_test.dart
+++ test/views/cart_item_row_test.dart
@@ -1,9 +1,27 @@
-
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sandwich_koullis/views/cart_item_row.dart';
+import 'package:sandwich_koullis/models/sandwich.dart';
+import 'package:sandwich_koullis/models/cart.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  // Dummy test to ensure the file is included in test coverage.
-  test('cart_item_row.dart loads', () {
-    expect(1, equals(1));
+  testWidgets('CartItemRow renders sandwich name', (WidgetTester tester) async {
+    final sandwich = Sandwich(type: SandwichType.veggieDelight, isFootlong: true, breadType: BreadType.white);
+    final item = CartItem(sandwich: sandwich, quantity: 1);
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Builder(
+          builder: (context) => CartItemRow(
+            item: item,
+            index: 0,
+            onIncrement: (i) {},
+            onDecrement: (i) {},
+            onRemove: (i) {},
+            onEdit: (i, s) {},
+          ),
+        ),
+      ),
+    );
+    expect(find.text(sandwich.name), findsOneWidget);
   });
 }
