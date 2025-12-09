f3df269d8d5563ae37c6ef5582a469e457260f53
Marinos Pierides <pieridesmarinos@gmail.com>
2025-12-09 18:12:54 +0000

    test: Add tests for AppStyles to verify font sizes and weights

--- test/views/app_styles_test.dart
+++ test/views/app_styles_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sandwich_koullis/views/app_styles.dart';
 
@@ -6,5 +7,21 @@ void main() {
     expect(heading1, isNotNull);
     expect(heading2, isNotNull);
     expect(normalText, isNotNull);
+  });
+
+  test('Base font size default', () {
+    expect(AppStyles.baseFontSize, 16.0);
+  });
+
+  test('TextStyles have correct font sizes', () {
+    expect(normalText.fontSize, 16.0);
+    expect(heading1.fontSize, 24.0);
+    expect(heading2.fontSize, 20.0);
+  });
+
+  test('TextStyles have correct font weights', () {
+    expect(heading1.fontWeight, FontWeight.bold);
+    expect(heading2.fontWeight, FontWeight.bold);
+    expect(normalText.fontWeight, isNot(FontWeight.bold));
   });
 }
